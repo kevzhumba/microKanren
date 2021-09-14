03785d139976b8427d02bc33b8b8d9602b9261d8
Kevin Zhang <kevinjianzhang@gmail.com>
2021-09-14 19:39:16 -0400

    Initial commit

---
+++ MicroKanren.scala
@@ -0,0 +1,75 @@
+package microKanren
+
+object MicroKanren {
+
+  type Goal = State => Stream 
+  type Subst = Map[Var, Val]
+
+  abstract trait Val
+  case class Var(index: Int) extends Val
+  case class IntWrapper(value: Int) extends Val
+  case class ListWrapper(list: List[Val]) extends Val
+
+  abstract trait Stream
+  case class MatureStream(state: State, stream: Stream) extends Stream
+  case class ImmatureStream(func: () => Stream) extends Stream
+  case class EmptyStream() extends Stream
+
+  case class State(s: Subst, c: Int)
+  case class UnificationException() extends Exception
+
+  def EmptyState(): State = State(Map(), 0)
+
+  def walk(u: Val, subst: Subst): Val = {
+    if (u.isInstanceOf[Var] && subst.contains(u.asInstanceOf[Var])) walk(subst((u.asInstanceOf[Var])), subst) else u
+  }
+
+  def extendSubst(x: Var, v: Val, s: Subst): Subst = s + (x -> v)
+
+  def unify(a: Val, b: Val, s: Subst): Subst = (walk(a, s), walk(b, s)) match {
+    case (Var(index1), Var(index2)) if (index1 == index2) => s
+    case (x: Var, v) => extendSubst(x, v, s)
+    case (v, x: Var) => extendSubst(x, v, s)
+    case (ListWrapper(list1), ListWrapper(list2)) if list1.length == list2.length => 
+      if (list1.isEmpty) s
+      val newS = unify(list1.head, list2.head, s) 
+      unify(ListWrapper(list1.tail), ListWrapper(list2.tail), newS)
+    case (u, v) if u == v => s
+    case (_, _) => throw UnificationException()
+  }
+
+  def call_fresh(f: Var => Goal): Goal = {
+    (state: State) => f(Var(state.c))(state.copy(c = state.c + 1))
+  }
+
+  def equality(a: Val, b: Val): Goal = {
+    (state: State) => try {
+      MatureStream(State(unify(a, b, state.s), state.c), EmptyStream())
+    } catch {
+      case UnificationException() => EmptyStream()
+    } 
+  }
+
+  def mplus(s1: Stream, s2: Stream): Stream = (s1, s2) match {
+    case (EmptyStream(), _) => s2
+    case (ImmatureStream(func), _) => ImmatureStream(() => mplus(s2, func()))
+    case (MatureStream(state, stream), _) => MatureStream(state, mplus(stream, s2))
+  }
+
+  def bind(s1: Stream, g: Goal): Stream = s1 match {
+    case EmptyStream() => EmptyStream()
+    case ImmatureStream(func) => ImmatureStream(() => bind(func(), g))
+    case MatureStream(state, stream) => mplus(g(state),  bind(stream, g))
+  }
+
+  def disj(g1: Goal, g2: Goal): Goal = {
+    (state: State) => mplus(g1(state), g2(state))
+  }
+
+  def conj(g1: Goal, g2: Goal): Goal = { 
+    (state: State) => bind(g1(state), g2)
+  }
+
+  
+
+}
